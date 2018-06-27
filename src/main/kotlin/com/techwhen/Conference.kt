e288bd1e1dfcecb06a94132380fc02eea5279897
Pam Rucinque <pamela.rucinque@gmail.com>
2018-06-27 18:20:36 +1000

    Hard code a response to return a single conference, add data class for conference plus jackson for parsing

---
+++ src/main/kotlin/com/techwhen/Conference.kt
@@ -0,0 +1,12 @@
+package com.techwhen
+
+import org.apache.log4j.Logger
+import java.util.*
+
+data class Conference(
+        val name: String,
+        val city: String,
+        val website: String,
+        val startDate: Date,
+        val endDate: Date)
+
--- src/main/kotlin/com/techwhen/Handler.kt
+++ src/main/kotlin/com/techwhen/Handler.kt
@@ -2,24 +2,30 @@ package com.techwhen
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.apache.log4j.Logger
+import java.time.Instant
 import java.util.*
 
 
 class Handler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
 
     override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
-        LOG.info("sdhfjkshdkfjdhsfklshfkljsh: $input")
+        LOG.info("This is what I got: $input")
 
+        val conference = Conference("KotlinConf",
+                "Melbourne",
+                "kotlinconf.com.au",
+                Date(Instant.now().toEpochMilli()),
+                Date(Instant.now().toEpochMilli()))
+        val mapper = jacksonObjectMapper()
 
         val headers = HashMap<String, String>()
         headers["X-Powered-By"] = "AWS Lambda & Serverless"
         headers["Content-Type"] = "application/json"
         return ApiGatewayResponse(
                 200,
-                """
-                | { "message": "Hello Chloe" }
-                """.trimMargin(),
+                mapper.writeValueAsString(conference),
                 headers,
                 true)
     }
